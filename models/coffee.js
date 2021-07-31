ba0228e7938009aafdac36af933526354669abe2
webgenapp <87999663+webgenapp@users.noreply.github.com>
2021-07-31 18:25:06 +0200

    Create app using webgen.dev

---
+++ models/coffee.js
@@ -0,0 +1,25 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Coffee extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Coffee.init(
+    {
+      name: DataTypes.STRING,
+      type: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Coffee',
+    }
+  )
+  return Coffee
+}
