eebd35fe49eb14d32ea199fc8c1892bbd6547ed5
webgenapp <87999663+webgenapp@users.noreply.github.com>
2021-07-31 18:25:16 +0200

    Create app using webgen.dev

---
+++ frontend/src/coffees/DetailCoffee.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import client from '../api'
+import { useQuery } from 'react-query'
+import { Coffee } from '../types'
+
+function DetailCoffee() {
+  const { id } = useParams<{ id: string }>()
+
+  const { data, isLoading } = useQuery<Coffee>(['coffees', id], () =>
+    client.get(`/api/v1/coffees/${id}`).then((response) => response.data)
+  )
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  const coffee = data as Coffee
+
+  return (
+    <div>
+      <label>{coffee.name}</label>
+      <br />
+
+      <label>{coffee.type}</label>
+      <br />
+    </div>
+  )
+}
+
+export default DetailCoffee
